39e0cb326a90567b9124a06e319263c378f5cc0e
QuentinTilak <lecavorzin.quentin@gmail.com>
2024-12-01 12:23:45 +0100

    exo 1 done

--- Exo2024_01.cs
+++ Exo2024_01.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("hello");
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace AoC2024_Exo01
 {
@@ -8,6 +9,62 @@ namespace AoC2024_Exo01
         {
             string text = Utils.ReadFile("E:\\Projets\\AdventOfCode\\AoC2024\\Exo2024_01.txt");
             string[] textSplitted = text.Split(Environment.NewLine);
+            List<int> leftIds = new List<int>();
+            List<int> rightIds = new List<int>();
+
+            foreach (string line in textSplitted)
+            {
+                string[] values = line.Split("   ");
+                leftIds.Add(Int32.Parse(values[0]));
+                rightIds.Add(Int32.Parse(values[1]));
+            }
+
+            // PART ONE
+            //Console.Write($"{GetAddedDistancesFromLists(leftIds.Order().ToList(), rightIds.Order().ToList())}");
+
+            // PART TWO
+            Console.Write($"{GetSumOfMultipliedOccurencesFromLists(leftIds, rightIds)}");
+        }
+
+        public static int GetSumOfMultipliedOccurencesFromLists(List<int> left, List<int> right)
+        {
+            Assert.IsTrue(left.Count == right.Count, "The lists don't have the same amount of elements");
+
+            int total = 0;
+
+            foreach (int value in left)
+            {
+                total += value * GetAmountOfOccurenceInList(value, right);
+            }
+
+            return total;
+        }
+
+        public static int GetAmountOfOccurenceInList(int searchValue, List<int> list)
+        {
+            int total = 0;
+
+            foreach (int value in list)
+            {
+                if (value == searchValue)
+                    total++;
+            }
+
+            return total;
+        }
+
+        public static int GetAddedDistancesFromLists(List<int> left, List<int> right)
+        {
+            Assert.IsTrue(left.Count == right.Count, "The lists don't have the same amount of elements");
+
+            int total = 0;
+
+            for (int i = 0; i < left.Count; i++)
+            {
+                total += Math.Abs(left[i] - right[i]);
+            }
+
+            return total;
         }
     }
 }
