9b54e93a61f5613d4e08c7706122f6dfaee6495a
QuentinTilak <lecavorzin.quentin@gmail.com>
2024-12-01 11:39:46 +0100

    project 2024 exo 01 setup

---
+++ Exo2024_01.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("hello");
+
+namespace AoC2024_Exo01
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string text = Utils.ReadFile("E:\\Projets\\AdventOfCode\\AoC2024\\Exo2024_01.txt");
+            string[] textSplitted = text.Split(Environment.NewLine);
+        }
+    }
+}
---
+++ Utils.cs
@@ -0,0 +1,11 @@
+﻿public static class Utils
+{
+    public static string ReadFile(string path)
+    {
+        string fileContent = File.ReadAllText(path)
+                    .TrimEnd('\n'); //Remove the last \n that is usually outside the scope of the puzzle
+
+        return fileContent;
+        //Console.WriteLine(fileContent);
+    }
+}
