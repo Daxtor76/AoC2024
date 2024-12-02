df9fe091a95e7f6186a605ec09c7ffd055492366
QuentinTilak <lecavorzin.quentin@gmail.com>
2024-12-02 10:55:29 +0100

    j2 creation

---
+++ Exo2024_02.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace AoC2024_Exo02
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string text = Utils.ReadFile("E:\\Projets\\AdventOfCode\\AoC2024\\Exo2024_02.txt");
+            string[] textSplitted = text.Split(Environment.NewLine);
+        }
+    }
+}
