355d599629cb291f1eddb9645ea734e5efde728b
QuentinTilak <lecavorzin.quentin@gmail.com>
2024-12-06 12:18:44 +0100

    get all necessary datas

--- Exo2024_05.cs
+++ Exo2024_05.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using System.Diagnostics;
 using System.Numerics;
 using System.Security.Permissions;
@@ -14,19 +15,57 @@ namespace AoC2024
 
         public void Launch()
         {
-            string[] textSplitted = text.Split(" ");
-            string[] rules = textSplitted[0].Split(Environment.NewLine);
-            List<OrderingRule> updates = GetAllUpdates(textSplitted[1]);
+            string[] textSplitted = text.Split(Environment.NewLine);
+            string[] tmpRules = textSplitted.TakeWhile(c => c.Length != 0).ToArray();
+            string[] tmpPages = GetTmpRules(textSplitted);
+            List<OrderingRule> rules = GetAllRules(tmpRules);
+            List<Pages> pages = GetAllPages(tmpPages);
 
-            
+            foreach (OrderingRule rule in rules)
+            {
+                Console.WriteLine($"Rule: {rule.concernedPage} - {rule.beforePage}");
+            }
+            foreach (Pages page in pages)
+            {
+                foreach (int nb in page.pages)
+                {
+                    Console.WriteLine($"Pages: {nb}");
+                }
+                Console.WriteLine($"---------------");
+            }
         }
 
-        public List<OrderingRule> GetAllUpdates(string entry)
+        public string[] GetTmpRules(string[] text)
         {
-            string[] updates = entry.Split(Environment.NewLine);
+            List<string> tmp = new List<string>();
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                if (text[i].Contains(","))
+                    tmp.Add(text[i]);
+            }
+
+            return tmp.ToArray();
+        }
+
+        public List<Pages> GetAllPages(string[] pages)
+        {
+            List<Pages> finalPages = new List<Pages>();
+
+            foreach (string s in pages)
+            {
+                Pages tmp = new Pages(s);
+
+                finalPages.Add(tmp);
+            }
+
+            return finalPages;
+        }
+
+        public List<OrderingRule> GetAllRules(string[] rules)
+        {
             List<OrderingRule> finalUpdates = new List<OrderingRule>();
 
-            foreach (string s in updates)
+            foreach (string s in rules)
             {
                 int concernedPage = Int32.Parse(s.Split("|")[0]);
                 int beforePage = Int32.Parse(s.Split("|")[1]);
@@ -36,6 +75,27 @@ namespace AoC2024
             }
 
             return finalUpdates;
+        }
+    }
+
+    public class Pages
+    {
+        public List<int> pages = new List<int>();
+
+        public Pages(string pages)
+        {
+            this.pages = SplitPages(pages);
+        }
+
+        public List<int> SplitPages(string pages)
+        {
+            string[] tmpPages = pages.Split(",");
+            List<int> tmp = new List<int>();
+
+            foreach (string s in tmpPages)
+                tmp.Add(Int32.Parse(s));
+
+            return tmp;
         }
     }
 
