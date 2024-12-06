db61895749952c9e1b6087c48a8e5740c7d1cdad
QuentinTilak <lecavorzin.quentin@gmail.com>
2024-12-06 10:53:18 +0100

    j5 p1 - setup data

--- Exo2024_05.cs
+++ Exo2024_05.cs
@@ -14,7 +14,40 @@ namespace AoC2024
 
         public void Launch()
         {
-            string[] textSplitted = text.Split(Environment.NewLine);
+            string[] textSplitted = text.Split(" ");
+            string[] rules = textSplitted[0].Split(Environment.NewLine);
+            List<OrderingRule> updates = GetAllUpdates(textSplitted[1]);
+
+            
+        }
+
+        public List<OrderingRule> GetAllUpdates(string entry)
+        {
+            string[] updates = entry.Split(Environment.NewLine);
+            List<OrderingRule> finalUpdates = new List<OrderingRule>();
+
+            foreach (string s in updates)
+            {
+                int concernedPage = Int32.Parse(s.Split("|")[0]);
+                int beforePage = Int32.Parse(s.Split("|")[1]);
+
+                OrderingRule tmp = new OrderingRule(concernedPage, beforePage);
+                finalUpdates.Add(tmp);
+            }
+
+            return finalUpdates;
+        }
+    }
+
+    public class OrderingRule
+    {
+        public int concernedPage;
+        public int beforePage;
+
+        public OrderingRule(int concernedPage, int beforePage)
+        {
+            this.concernedPage = concernedPage;
+            this.beforePage = beforePage;
         }
     }
 }
